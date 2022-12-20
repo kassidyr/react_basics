7b478bbe7d36ae98219f8d538eae0851c018f44b
kassidyr <kassidy.ralph@gmail.com>
2022-12-20 13:06:27 -0500

    Week 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import Card from "./components/Card";
+import React from 'react';
+import Fruits from './components/Fruits';
+import FruitsCounter from './components/FruitsCounter';
 
 function App() {
+  const [fruits] = React.useState([
+    { fruitName: 'apple', id: 1 },
+    { fruitName: 'apple', id: 2 },
+    { fruitName: 'plum', id: 3 }
+  ]);
+
   return (
     <div className="App">
-      <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3"/>
-      <Card h2="Second card's h2" h3="Second card's h3"/>
-      <Card h2="Third card's h2" h3="Third card's h3"/>
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits}/>
+      <FruitsCounter fruits={fruits}/>
     </div>
   );
 }
